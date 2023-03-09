90f8f6b223c1f087a64dea369e9253db11848a20
viggeh2000 <79460359+VictorHylen@users.noreply.github.com>
2023-03-09 13:03:24 +0100

    webiste dynamic with firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,134 +15,156 @@ import ReactSwitch from 'react-switch';
 
 
 function Home(){
-    const [checked, setChecked] = useState(true);
+const [lightStatus, setLightStatus] = useState(false);
+const [doorStatus, setDoorStatus] = useState(false);
 
-    const handleChange = (checked) => {
-        setChecked(checked);
-    }
-
-    const [door, setDoor] = useState(true);
-
-    const handleDoor = (checked) => {
-        setDoor(checked);
-    }
-
-    const [Units, setUnits] = useState([])
-  const [loading, setloading] = useState([false])
+//Firebase
+const [Units, setUnits] = useState([]);
+const [loading, setloading] = useState(false);
 
   const ref = firebase.firestore().collection("Units")
   console.log(ref);
 
-  function getUnits(){
-    setloading(true)
-    ref.onSnapshot((querySnapshot) =>{
-      const items = []
-      querySnapshot.forEach((doc) =>{
-        items.push(doc.data())
-      })
-      setUnits(items)
-      setloading(false)
 
-    })
+
+  function getUnits() {
+    setloading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        const unit = doc.data();
+        if (unit.title === "lamp1") {
+          const status = unit.status === 0 ? false : true; // if the value is 0 then lamp of
+          setLightStatus(status);
+        } else if (unit.title === "door1") {
+          const status = unit.status === 0 ? false : true; // if the door is 1 then door opend
+          setDoorStatus(status);
+        }
+        items.push(unit);
+      });
+      setUnits(items);
+      setloading(false);
+    });
   }
 
   useEffect(() =>{
     getUnits()
   }, [])
-  if(loading){
-    return <h1>Loading...</h1>
-  }
+
+
+    if(loading){
+        return <h1>Loading...</h1>
+    }
+
+    const handleUnit = (unit, checked, document) => {
+        console.log('Handling unit:', unit);
+        console.log('Document:', document);
+        const newStatus = checked ? 1 : 0;
+        const collectionRef = firebase.firestore().collection("Units").doc(document);
+
+        collectionRef.get()
+          .then((doc) => {
+            if (doc.exists) {
+              collectionRef.update({ status: newStatus })
+                .then(() => console.log('Unit status updated'))
+                .catch((error) => console.error('Error updating unit status:', error))
+            } else {
+              console.error('Unit not found in database');
+            }
+          })
+          .catch((error) => console.error('Error reading unit from database:', error))
+      } 
+      
+    
+
+
 
 
     return (
         <div>
-        <div className="center">
-        <br />
-        <h1 className="shadow"> Unit-Computer </h1>
-        <br />
+    <div className="center">
+      <br />
+      <h1 className="shadow"> Unit-Computer </h1>
+      <br />
 
-        <h2 className="description"> Light </h2>
-        </div>
-        <div className="border">
-            <br />
-        <ReactSwitch
-            checked={checked}
-            onChange={handleChange}
-            onColor="#86d3ff"
-            onHandleColor="#2693e6"
-            handleDiameter={30}
-            uncheckedIcon={false}
-            checkedIcon={false}
-            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-            width={48}
-            height={20}
-            id="material-switch"
-        />
-        {checked &&
+      <h2 className="description"> Light </h2>
+    </div>
+    <div className="border">
+      <br />
+      <ReactSwitch
+        checked={lightStatus}
+        onChange={(checked) => handleUnit({ title: "lamp1" }, checked, "Lamp")}
+        onColor="#86d3ff"
+        onHandleColor="#2693e6"
+        handleDiameter={30}
+        uncheckedIcon={false}
+        checkedIcon={false}
+        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+        width={48}
+        height={20}
+        id="material-switch"
+      />
+      {lightStatus &&
         <div>
-        <h3 className="status"> On </h3>
-        <img src={light_on} alt="light_on" width="60" height="75" />
+          <h3 className="status"> On </h3>
+          <img src={light_on} alt="light_on" width="60" height="75" />
         </div>
-        }
-        {!checked &&
+      }
+      {!lightStatus &&
         <div>
-        <h3 className="status"> Off </h3>
-        <img src={light_off} alt="light_off" width="60" height="75" />
+          <h3 className="status" style={{ color: "red" }}> Off </h3>
+          <img src={light_off} alt="light_off" width="60" height="75" />
         </div>
-        }
+      }
         <br />
         </div>
-        
-
         <br />
         <div className="center">
         <h2 className="description"> Door </h2>
         </div>
         <div className="border">
             <br />
-        <ReactSwitch
-            checked={door}
-            onChange={handleDoor}
-            onColor="#86d3ff"
-            onHandleColor="#2693e6"
-            handleDiameter={30}
-            uncheckedIcon={false}
-            checkedIcon={false}
-            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-            width={48}
-            height={20}
-            id="material-switch"
-        />
-        {door &&
-        <div>
-        <h3 className="status"> Open </h3>
-        <img src={door_open} alt="light_on" width="80" height="150" />
-        </div>
-        }
-        {!door &&
-        <div>
-        <h3 className="status"> Closed </h3>
-        <img src={door_closed} alt="light_off" width="80" height="150" />
-        </div>
-        }
-        <br />
-        </div>
-
-    
-    
-        <br />
+            <ReactSwitch
+              checked={doorStatus}
+              onChange={(checked) => handleUnit({ title: "door1" }, checked, "Door")}
+              onColor="#86d3ff"
+              onHandleColor="#2693e6"
+              handleDiameter={30}
+              uncheckedIcon={false}
+              checkedIcon={false}
+              boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+              activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+              width={48}
+              height={20}
+              id="material-switch"
+            />
+            {doorStatus &&
+              <div>
+                <h3 className="status"> Open </h3>
+                <img src={door_open} alt="door_open" width="60" height="75" />
+              </div>
+            }
+            {!doorStatus &&
+              <div>
+                <h3 className="status" style={{ color: "red" }}> Closed </h3>
+                <img src={door_closed} alt="door_closed" width="60" height="75" />
+              </div>
+            }
+            <br />
+          </div>
+      
+          <br />
         <div className="center">
         <h2 className="description"> Units </h2>
         </div>
         <div className="border">
-        {Units.map((Units) =>(
-        <div key={Units.id}>
-          <h2>Enhet: {Units.title}</h2>
-          <h2>Status: {Units.status}</h2>
-          </div>
-        ))}
+        {Units.map((unit, index) => (
+            <div key={index}>
+                <h2>Enhet: {unit.title}</h2>
+                <h2>Status: {unit.status}</h2>
+            </div>
+            ))}
         </div>
         <br />
 
