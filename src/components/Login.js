38ee2716fb47418a1ada04faa148944f5e822e54
Timmy Bergvall <79316874+TimmyBergvall@users.noreply.github.com>
2023-03-23 23:13:45 +0100

    Login created.
    Only fake password. Startup/test

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,56 @@
-import Home from "./Home";
-
 import { useNavigate } from "react-router";
+import React, { useState } from "react";
+import "../style/Login.css";
 
 function Login() {
-
+  const usernameDb = "admin";
+  const passwordDb = "admin";
+  
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
   const handleLogin = () => {
-    navigate("/home");
+    if (username === usernameDb && password === passwordDb) {
+      navigate("/home");
+    }
+    else {
+      alert("Wrong username or password");
+    }
   };
 
 
-
     return (
-      <div className="center">
+      <div className="login">
         <h1>Sign In</h1>
-        <button onClick={handleLogin}>Sign in</button>
 
-    </div>
+        <form onSubmit={handleLogin}>
+          <label>
+            <p>Username</p>
+            <input type="text" value={username} onChange={handleUsername} />
+          </label>
+          <label>
+            <p>Password</p>
+            <input type="password" value={password} onChange={handlePassword} />
+          </label>
+        <div> <br></br>
+          <button type="submit">Login</button>
+        </div>
+        </form>
+      </div>
 
 
         
     );
 };
+
   
 export default Login;
