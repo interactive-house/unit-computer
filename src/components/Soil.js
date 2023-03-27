357aee9a3a80e56de17654dc1796a787e7972fa3
viggeh2000 <79460359+VictorHylen@users.noreply.github.com>
2023-03-27 14:08:52 +0200

    changed soil

--- src/components/Soil.js
+++ src/components/Soil.js
@@ -3,45 +3,52 @@ import "firebase/database";
 import firebase from "./firebase";
 import soil_wet from "../media/soil_wet.png";
 import soil_dry from "../media/soil_dry.png";
+import soil_perfect from "../media/soil_perfect.png";
 
 function Soil() {
-  const [Status, setStatus] = useState(false);
+  const [status, setStatus] = useState("");
 
   useEffect(() => {
-    const Ref = firebase.database().ref("SmartHomeValueSoil/StatusOfSoil");
+    const ref = firebase.database().ref("SmartHomeValueSoil/StatusOfSoil");
 
-    Ref.on("value", (snapshot) => {
-      setStatus(snapshot.val() === "wet");
+    ref.on("value", (snapshot) => {
+      setStatus(snapshot.val());
     });
 
     return () => {
-      Ref.off();
+      ref.off();
     };
   }, []);
 
   return (
     <div className="center">
-      <h2 className="description"> Soil </h2>
+      <h2 className="description">Soil</h2>
       <div className="border">
         <br />
-        {Status && (
+        {status === "Wet" && (
           <div>
-            <h3 className="statussoil"> Wet </h3>
+            <h3 className="statussoil">Wet</h3>
             <img src={soil_wet} alt="soil_wet" width="130" height="130" />
           </div>
         )}
-        {!Status && (
+        {status === "Dry" && (
           <div>
             <h3 className="status" style={{ color: "#b2996e" }}>
-              {" "}
-              Dry{" "}
+              Dry
             </h3>
             <img src={soil_dry} alt="soil_dry" width="130" height="130" />
           </div>
         )}
+        {status === "Perfect" && (
+          <div>
+            <h3 className="status" style={{ color: "#90ee90" }}>
+              Perfect
+            </h3>
+            <img src={soil_perfect} alt="soil_perfect" width="130" height="130" />
+          </div>
+        )}
         <br />
       </div>
-
       <br />
     </div>
   );
