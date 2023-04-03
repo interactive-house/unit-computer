b0712973c6acee1b8036801916c0fa02247c2272
viggeh2000 <79460359+VictorHylen@users.noreply.github.com>
2023-04-03 13:00:08 +0200

    made the webiste more smooth

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "../style/Style.css";
 import Light from "./Light";
 import Door from "./Door";
@@ -5,17 +6,48 @@ import Window from "./Window";
 import Soil from "./Soil";
 import SimulatedDevice from "./SimulatedDevice";
 import Navbar from "./NavBar";
+import ReactLoading from "react-loading";
 
 function Home() {
+  const [showBuffer, setShowBuffer] = useState(true);
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowBuffer(false);
+      setShowContent(true);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
+
+
+
   return (
     <div>
-      <Navbar />
-      <Light />
-      <Door />
-      <Window />
-      <SimulatedDevice />
-      <Soil />
-      <br />
+      {showBuffer && (
+        <div className="buffer-container">
+        <ReactLoading
+        type="spin"
+        color="rgb(0, 255, 89)"
+        height={100}
+        width={100}
+        
+      />
+
+        </div>
+      )}
+      <div className={`content-container${showContent ? " show" : ""}`}>
+        <Navbar />
+        <Light />
+        <Door />
+        <Window />
+        <SimulatedDevice />
+        <Soil />
+        <br />
+      </div>
     </div>
   );
 }
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,17 +4,23 @@ import "firebase/compat/firestore";
 import "firebase/compat/database";
 import "firebase/compat/storage";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCOZcBxs3RkxuxDrf5vT2HwexFh3ZCw94c",
-  authDomain: "smarthome-3bb7b.firebaseapp.com",
-  databaseURL: "https://smarthome-3bb7b-default-rtdb.firebaseio.com",
-  projectId: "smarthome-3bb7b",
-  storageBucket: "smarthome-3bb7b.appspot.com",
-  messagingSenderId: "642589059290",
-  appId: "1:642589059290:web:bf23df5327b9e305c81de3",
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyCOZcBxs3RkxuxDrf5vT2HwexFh3ZCw94c",
+    authDomain: "smarthome-3bb7b.firebaseapp.com",
+    databaseURL: "https://smarthome-3bb7b-default-rtdb.firebaseio.com",
+    projectId: "smarthome-3bb7b",
+    storageBucket: "smarthome-3bb7b.appspot.com",
+    messagingSenderId: "642589059290",
+    appId: "1:642589059290:web:bf23df5327b9e305c81de3",
+  };
 
-firebase.initializeApp(firebaseConfig);
+try {
+  firebase.initializeApp(firebaseConfig);
+} catch (error) {
+  console.log("Firebase initialization error", error.stack);
+}
+
+
 
 export default firebase;
+
